55ccfa0691c63c412cff849022ba5c51d4ab34b0
Albert Chen <algebraprodigy@gmail.com>
2016-02-29 14:53:47 +0800

    default image shows if none has been uploaded

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,2 +1,10 @@
 module LocationsHelper
+		def location_img_select(location)
+		return image_tag location.header_image.url(:medium),
+		        id: 'image-preview',
+		        class: 'img-responsive center-block' if location.header_image.exists?
+					image_tag 'default-location.jpg',
+						id: 'image-preview',
+            class: 'img-responsive center-block'
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
 	validates :user_name, presence: true, length: { minimum: 2 }
 	validates :phone_number, presence: true
+
+	# avatar image for user
 	has_attached_file :avatar, styles: { medium: '152 x 152#' }
 	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
